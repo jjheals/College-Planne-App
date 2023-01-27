a12290001b15f7a42085411dd0f5d6e7a8b2a419
jjhealey <jjhealey19@gmail.com>
2023-01-27 17:34:02 -0500

    Updates

---
+++ HoyaHacks-2023-College-Planner-App/SideMenu/SideMenuViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  SideMenuViewModel.swift
+//  College-Planner-App-SwiftUI
+//
+//  Created by Anay Gandhi on 1/27/23.
+//
+
+import Foundation
+import SwiftUI
+
+//needs to be changed to data model
+enum SideMenuViewModel: Int, CaseIterable {
+    case dashboard
+    case calendar
+    case toDoList
+    
+    var title: String {
+        switch self {
+        case .dashboard: return "Dashboard"
+        case .calendar: return "Calendar"
+        case .toDoList: return "To-Do List"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .dashboard: return "house"
+        case .calendar: return "calendar.circle"
+        case .toDoList: return "list.clipboard"
+        }
+    }
+    
+    
+}
+
