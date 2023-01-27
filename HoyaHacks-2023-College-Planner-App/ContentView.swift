1a25fc567dd169b29cf9f1052cf68438c352b197
jjhealey <jjhealey19@gmail.com>
2023-01-27 17:29:45 -0500

    Initial Commit

---
+++ HoyaHacks-2023-College-Planner-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HoyaHacks-2023-College-Planner-App
+//
+//  Created by Justin Healey on 1/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HoyaHacks-2023-College-Planner-App/HoyaHacks_2023_College_Planner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoyaHacks_2023_College_Planner_AppApp.swift
+//  HoyaHacks-2023-College-Planner-App
+//
+//  Created by Justin Healey on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HoyaHacks_2023_College_Planner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
