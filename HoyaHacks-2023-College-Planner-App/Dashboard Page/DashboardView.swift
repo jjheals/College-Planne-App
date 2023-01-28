f7bfe380c60af6529c46232df43189ee37940a8f
Anay Gandhi <anaygandhi@gmail.com>
2023-01-27 23:45:32 -0500

    organized editButton for dashboardView

--- HoyaHacks-2023-College-Planner-App/Dashboard Page/DashboardView.swift
+++ HoyaHacks-2023-College-Planner-App/Dashboard Page/DashboardView.swift
@@ -22,45 +22,7 @@ struct DashboardView_UI: View {
                 }
             }
         }
-        EditButtonDashboardView()
-    }
-}
-
-struct EditButtonDashboardView: View {
-    @State private var ShowTextField = false
-    @State private var enteredSubject: String = ""
-    
-    var body: some View {
-        if ShowTextField {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.blue)
-                    .frame(width: 200, height: 100)
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.white)
-                    .frame(width: 180, height: 80)
-                VStack (alignment: .center, spacing: 15) {
-                    TextField("Enter Subject", text: $enteredSubject)
-                        .multilineTextAlignment(.center)
-                }
-            }
-        }
-        
-        VStack {
-            
-            HStack {
-                Spacer()
-                Button(action: {
-                    self.ShowTextField.toggle()
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                        .foregroundColor(.blue)
-                        .font(.system(size:48))
-                        .frame(maxWidth: .infinity, minHeight: 0, alignment: .trailing)
-                        .padding()
-                }
-            }
-        }
+        EditButtonDashboard()
     }
 }
 
