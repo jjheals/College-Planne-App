ea568576d8fb347c60ffd697b8ab6d3f42650239
Anay Gandhi <anaygandhi@gmail.com>
2023-01-28 00:01:15 -0500

    some request stuff

---
+++ HoyaHacks-2023-College-Planner-App/Requests/HTTPResponse.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPResponse.swift
+//  HoyaHacks-2023-College-Planner-App
+//
+//  Created by Anay Gandhi on 1/28/23.
+//
+
+import Foundation
+
+struct HTTPResponse: Decodable {
+    
+}
+
+
