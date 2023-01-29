ca61bc8dd182dd94e05fc1491930070f4ed53e7d
Justin Healey <93351901+jjheals@users.noreply.github.com>
2023-01-29 07:07:33 -0500

    Update Alert.py
    Added documentation

--- ServerFramework/SwiftSMS/Alert.py
+++ ServerFramework/SwiftSMS/Alert.py
@@ -14,10 +14,11 @@ class Alert:
     
     # Credentials
     # NOTE: storing in env variables is more secure but this was for testing purposes only
-    accountSID = 'ACa6acd5e112dd01c696b4535e8737a1b9' # Redacted 
-    authToken = '6a944ad16d8021fed62d060c4960d8d6' # Redacted 
-    messageSID = 'MGd985d37adb1436dab1829cf15dd8329b'
-    fromNumber = '+184444361861'
+    # Creds were also changed and keys deleted after initial development
+    accountSID = '' # Redacted 
+    authToken = '' # Redacted 
+    messageSID = ''
+    fromNumber = ''
     client = Client(accountSID, authToken)
     sendToNumber: str
     alert: str
@@ -29,7 +30,7 @@ class Alert:
     # Send an alert to a user
     # Uses the variables in self to specify configurations
     def sendAlert(self):
-        message = self.client.messages.create(from_='+16402238774',
+        message = self.client.messages.create(from_='',
                                          messaging_service_sid = self.messageSID,
                                          body = self.alert,      
                                          to = self.sendToNumber) # Static for testing 
