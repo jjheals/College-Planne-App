51ad7e3f38c2f5fe9aa01773b49d4cb4cdada7cf
Justin Healey <93351901+jjheals@users.noreply.github.com>
2023-01-28 20:55:01 -0500

    Files from remote server (edited locally)
    Updated classes to better reflect DB model; added get-assignments, add-assignment functionalities; fixed return formatting issues; restructued main.py and imports

--- ServerFramework/functions.py
+++ ServerFramework/functions.py
@@ -5,13 +5,13 @@ import data.classes.Assignment as assn
 # Returns a datetime object 
 # date must be given in mm-dd-yyyy format
 def convertDate(date: str): 
-    
+    print(f"DATE {date}")
     # date is given in mm-dd-yyyy
-    dateArray = date.split()
-    mm = int(date[:2])
-    dd = int(dateArray[3]+dateArray[4])
-    yyyy = int(date[4:])
+    mm = int(date[0]+date[1])
+    dd = int(date[3]+date[4])
+    yyyy = int(date[6]+date[7]+date[8]+date[9])
     
     # Convert the month, day, and year into dayOfYear and year
     dateObj = datetime(yyyy, mm, dd)
-    return dateObj
+    return dateObj 
+    
